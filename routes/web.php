5e318760fc9fc4ef60b260b188f7fb1cf25cf7ef
Tuấn Nam <teddy.nam152@gmail.com>
2024-07-25 19:01:38 +0700

    [Lab5] PH31298

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,6 @@ use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-use App\Models\Post;
-use \App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
